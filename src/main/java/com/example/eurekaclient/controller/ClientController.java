766c405e57562d52c091eca8b2b45d0c7e81c7bd
武海军 <hongxiuzhao2016@163.com>
2019-09-11 21:13:08 +0800

    init

---
+++ src/main/java/com/example/eurekaclient/controller/ClientController.java
@@ -0,0 +1,35 @@
+package com.example.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ClientController {
+
+//    @Autowired
+//     private LoadBalancerClient loadBalancerClient;
+     @Autowired
+     private RestTemplate restTemplate;
+    @GetMapping("/getProductMsg")
+    public String getProductMsg(){
+        //1. 第一种方式
+//        RestTemplate  restTemplate = new RestTemplate();
+//       String  response =  restTemplate.getForObject("http://localhost:8761/msg",String.class);
+       // 2.第二种方式
+//        RestTemplate  restTemplate = new RestTemplate();
+//        ServiceInstance product = loadBalancerClient.choose("PRODUCT");
+//
+//        String url = String.format("http://%s:%s", product.getHost(), product.getPort()) + "/msg";
+//        String  response =  restTemplate.getForObject(url,String.class);
+        // 3. 第三种方式
+        String response = restTemplate.getForObject("http://PRODUCT/msg", String.class);
+        return response;
+    }
+
+
+}
+
